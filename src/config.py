e25cdb0fe4615a830341f709b3dc6f9cb427d354
DevWadson <marlonwadson.dev@gmail.com>
2025-10-12 17:23:45 -0300

    chore(config): centralizar instâncias de variáveis globais e sessão do banco
    fix(config): correção de imports para refletir estrutura "src/database"

--- src/config.py
+++ src/config.py
@@ -1,34 +1,9 @@
 """ARQUIVO DE CONFIGURAÇÕES.(_config)"""
 #==========IMPORTS==========
-import os
-from flask import Flask
-from flask_mysqldb import MySQL
-from dotenv import load_dotenv
-from database.SQLite.sqlite_db_script import Coordenada
-from database.SQL.mysql_conection import conectar_mysql
+from sqlalchemy.orm import Session
+from src.database import Coordenada
+from src.database.SQL import get_mysql_db
 
 #==========Declarações Globais==========
-load_dotenv()
-app = Flask(__name__)
-mysql = MySQL()
-
-#'/_main/'
-class MySQLConfig:
-    """Configurações do MySQL"""
-    def iniciar(self):
-        """Gerencia a configuração com o banco"""
-        host = os.getenv("DB_HOST")
-        user = os.getenv("DB_USER")
-        psswrd = os.getenv("DB_PSSWRD")
-        name = os.getenv("DB_NAME")
-
-        app.config['MYSQL_HOST'] = host
-        app.config['MYSQL_USER'] = user
-        app.config['MYSQL_PASSWORD'] = psswrd
-        app.config['MYSQL_DB'] = name
-
-        return app
-
-db_config = MySQLConfig()
-session = conectar_mysql()
+session: Session = next(get_mysql_db()) #SQL
 coordenada = session.query(Coordenada).all()
