cb12dcc394c60a2a4154bef30fc2fe9241075dac
DevWadson <marlonwadson.dev@gmail.com>
2025-10-12 19:18:01 -0300

    refactor(coordenada_mine_script): renomar ForeignKey(id_dimen) para id_dim
    refactor(coordenada_mine_script): renomar ForeignKey(id_dimensao) para id_dim

--- src/database/SQL/Coordenadas_Mine_script.sql
+++ src/database/SQL/Coordenadas_Mine_script.sql
@@ -20,23 +20,23 @@ CREATE TABLE IF NOT EXISTS dimensao(
 CREATE TABLE IF NOT EXISTS local(
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     id_server INT UNSIGNED,
-    id_dimen INT UNSIGNED,
+    id_dim INT UNSIGNED,
     nome VARCHAR(50),
     FOREIGN KEY (id_server) REFERENCES servidor(id), -- O local está no servidor criado
-    FOREIGN KEY (id_dimen) REFERENCES dimensao(id) -- O local está na dimensão que o personagem está, que está no servidor criado
+    FOREIGN KEY (id_dim) REFERENCES dimensao(id) -- O local está na dimensão que o personagem está, que está no servidor criado
 );
 
 -- Tabela que indica a posição geográfica do personagem
 CREATE TABLE IF NOT EXISTS coordenada(
 	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     id_server INT UNSIGNED,
-    id_dimensao INT UNSIGNED,
+    id_dim INT UNSIGNED,
     id_local INT UNSIGNED,
     x FLOAT,
     y FLOAT,
     z FLOAT,
     FOREIGN KEY (id_server) REFERENCES servidor(id), -- A coordenada está no servidor criado
-    FOREIGN KEY (id_dimensao) REFERENCES dimensao(id), -- A coordenada está na dimensão definida, que está no servidor
+    FOREIGN KEY (id_dim) REFERENCES dimensao(id), -- A coordenada está na dimensão definida, que está no servidor
     FOREIGN KEY (id_local) REFERENCES local(id) -- A coordenada referencia o local do personagem, que está no servidor
 );
 
