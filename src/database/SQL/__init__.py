b6921277dd5be2df192be9d3e04cf1e8800b1d75
DevWadson <marlonwadson.dev@gmail.com>
2025-10-12 12:22:40 -0300

    chore(SQL): Atualizado o arquivo __init__.py para expor as entidades de '/models' e '/schemas'

--- src/database/SQL/__init__.py
+++ src/database/SQL/__init__.py
@@ -1,3 +1,6 @@
 """
 Pacote SQL contendo scripts de criação e conexão do banco de dados.
 """
+from . import models
+from . import schemas
+from .mysql_connection import get_mysql_db, Base, SessionLocal, MySQL_BIN
