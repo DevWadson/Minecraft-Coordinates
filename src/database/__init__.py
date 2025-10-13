ecd3e1d0f6630db69c1d1f8707ac67f0d8c3c65b
DevWadson <marlonwadson.dev@gmail.com>
2025-10-13 19:45:50 -0300

    refactor(database): simplificar importação de SQLite

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -5,16 +5,4 @@ Importa e centraliza os objetos essenciais de conexão, modelos e schemas
 utilizados em toda a aplicação para facilitar o acesso e organização.
 """
 from . import SQL
-
-from .SQLite import (
-    Servidor,
-    Dimensao,
-    Local,
-    Coordenada,
-    criar_tabelas_sqlite,
-    conectar_sqlite_bd,
-    commit_coordenada,
-    check_existence,
-    SQLite_BIN,       # engine SQLite
-    Base              # declarative_base
-)
+from . import SQLite
