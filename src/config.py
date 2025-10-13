2b50d63ada5980c8e2f72abb8d7c53b6f09c48e3
DevWadson <marlonwadson.dev@gmail.com>
2025-10-13 19:42:25 -0300

    fix(config): corrigir import para Coordenada

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 """ARQUIVO DE CONFIGURAÇÕES.(_config)"""
 #==========IMPORTS==========
 from sqlalchemy.orm import Session
-from src.database import Coordenada
+from src.database.SQL.models import Coordenada
 from src.database.SQL import get_mysql_db
 
 #==========Declarações Globais==========
