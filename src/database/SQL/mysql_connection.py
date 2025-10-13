49b0771b7f6abebb7e4603f4acce15545adaa084
DevWadson <marlonwadson.dev@gmail.com>
2025-10-13 19:54:11 -0300

    refactor(mysql_connection): retirar importação inútil
    feat(mysql_connection): adicionar suporte a commit para Servidor, Dimensao, Local e Coordenada

--- src/database/SQL/mysql_connection.py
+++ src/database/SQL/mysql_connection.py
@@ -16,19 +16,13 @@ from dotenv import load_dotenv
 from src.database.SQL.base import Base
 from src.database.SQL.models import Servidor, Dimensao, Local, Coordenada
 
-from src.database.SQL.schemas import (
-    ServidorBase, ServidorCreate, ServidorResponse,
-    DimensaoBase, DimensaoCreate, DimensaoResponse,
-    LocalBase, LocalCreate, LocalResponse,
-    CoordenadaBase, CoordenadaCreate, CooredenadaResponse
-)
-
 load_dotenv()
 
 MYSQL_CONN = f"mysql+pymysql://{os.getenv("DB_USER")}:{os.getenv("DB_PSSWRD")}@{os.getenv("DB_HOST")}/{os.getenv("DB_NAME")}"
 MySQL_BIN = create_engine(MYSQL_CONN, echo=True)
 SessionLocal = sessionmaker(bind=MySQL_BIN)
 
+#==========Conexão com o banco MySQL==========
 def get_mysql_db() -> Generator[Session, None, None]:
     """Retorna uma sessão SQLAlchemy"""
     db = SessionLocal()
@@ -39,7 +33,48 @@ def get_mysql_db() -> Generator[Session, None, None]:
     finally:
         db.close()
 
-Base.metadata.create_all(bind=MySQL_BIN)
+#==========Criação das tabelas==========
+def criar_tabelas_mysql():
+    """Cria as tabelas no banco MySQL."""
+    Base.metadata.create_all(bind=MySQL_BIN)
 
+#==========Métodos para o banco MySQL==========
+def commit_servidor(servidor: Servidor) -> Servidor:
+    """Comitta o servidor no banco MySQL."""
+    with get_mysql_db() as db:
+        db.add(servidor)
+        db.commit(servidor)
+        db.refresh(servidor)
+
+    return servidor
+
+def commit_dimensao(dimensao: Dimensao) -> Dimensao:
+    """Comitta a dimensão no banco MySQL"""
+    with get_mysql_db() as db:
+        db.add(dimensao)
+        db.commit(dimensao)
+        db.refresh(dimensao)
+
+    return dimensao
+
+def commit_local(local: Local) -> Local:
+    """Comitta o local no banco MySQL"""
+    with get_mysql_db() as db:
+        db.add(local)
+        db.commit(local)
+        db.refresh(local)
+
+    return local
+
+def commit_coordenada(coordenada: Coordenada) -> Coordenada:
+    """Comitta a coordenada no banco MySQL"""
+    with get_mysql_db() as db:
+        db.add(coordenada)
+        db.commit(coordenada)
+        db.refresh(coordenada)
+
+    return coordenada
+
+#==========Teste de conexão==========
 with MySQL_BIN.connect() as conexao:
     print("Conexão bem sucedida!")
