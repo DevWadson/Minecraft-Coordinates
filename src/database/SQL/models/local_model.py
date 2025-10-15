e9b9e08bab5bfa6a55d9a5fe22f387e853cb2115
DevWadson <marlonwadson.dev@gmail.com>
2025-10-15 20:20:24 -0300

    feat(LocalModel): adicionar relacionamento entre Local, Dimensão e Coordenada

--- src/database/SQL/models/local_model.py
+++ src/database/SQL/models/local_model.py
@@ -1,5 +1,6 @@
 """ . """
 from sqlalchemy import Column, Integer, VARCHAR, ForeignKey
+from sqlalchemy.orm import relationship
 from src.database.SQL.base import Base
 
 class Local(Base):
@@ -10,3 +11,8 @@ class Local(Base):
     id_server = Column(Integer, ForeignKey("servidor.id"))
     id_dim = Column(Integer, ForeignKey("dimensao.id"))
     nome = Column(VARCHAR(50), nullable=False)
+
+    #Seleciona as dimensões do local
+    #Seleciona as coordenadas do local
+    dimension = relationship("Dimensao", back_populates="local_name")
+    coord = relationship("Coordenada", back_populates="local")
