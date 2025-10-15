4c364b06ce516dd770d48b4bdc8fc897a80b616e
DevWadson <marlonwadson.dev@gmail.com>
2025-10-15 20:16:38 -0300

    feat(ServidorModel): adicionar relacionamento entre Servidor e Dimensão

--- src/database/SQL/models/servidor_model.py
+++ src/database/SQL/models/servidor_model.py
@@ -1,10 +1,14 @@
 """ . """
 from sqlalchemy import Column, Integer, VARCHAR
+from sqlalchemy.orm import relationship
 from src.database.SQL.base import Base
 
 class Servidor(Base):
     """Tabela que representa o mundo/servidor criado"""
     __tablename__ = "servidor"
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     nome = Column(VARCHAR(120), nullable=False)
+
+    #Seleciona as dimensões do servidor
+    dimension = relationship("Dimensao")
