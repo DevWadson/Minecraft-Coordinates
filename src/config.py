ec9a3633cf2d74da04cb4ece1aaf77bf83312e84
DevWadson <marlonwadson.dev@gmail.com>
2025-10-15 21:52:00 -0300

    refactor(config): remover acesso global à sessão e adotar get_mysql_db() como context manager

--- src/config.py
+++ src/config.py
@@ -1,9 +1,5 @@
 """ARQUIVO DE CONFIGURAÇÕES.(_config)"""
 #==========IMPORTS==========
-from sqlalchemy.orm import Session
-from src.database.SQL.models import Coordenada
-from src.database.SQL import get_mysql_db
+
 
 #==========Declarações Globais==========
-session: Session = next(get_mysql_db()) #SQL
-coordenada = session.query(Coordenada).all()
