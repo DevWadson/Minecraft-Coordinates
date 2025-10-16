8ead0c9ae5082ef5fe695b5ad8b91bed356d4859
DevWadson <marlonwadson.dev@gmail.com>
2025-10-15 21:48:01 -0300

    chore(gui): renomear método "salvar_coord" para refletir sua responsabilidade real em "botao_save"
    refactor(gui): definir explicitamente o uso de Schemas na validação dos campos de "coletar_coor()"
    
    refactor(gui): reestruturar salvamento com uso de Schemas, de Servidor, Dimensao, Local, Coordenada em "salvar_coord_info()"
    
    refactor(gui): adaptar validação de campos para usar schemas em "validate_entries()"

--- src/gui.py
+++ src/gui.py
@@ -1,9 +1,17 @@
 """ARQUIVO REFATORADO QUE REPRESENTA A GUI DO PROGRAMA (_gui)."""
 import customtkinter as ctk
 import pandas as pd
-from src.database import commit_coordenada, check_existence
+from src.database.SQL.models import Servidor, Dimensao, Local, Coordenada
+from src.database.SQL.schemas import ServidorCreate, DimensaoCreate, LocalCreate, CoordenadaCreate
+from src.database.SQL.mysql_connection import (
+    commit_servidor,
+    commit_dimensao,
+    commit_local,
+    commit_coordenada,
+    SessionLocal
+)
+# from src.database.SQLite import commit_coordenada, check_existence
 from .utils import buscar_local, clear_frame, criar_botao, selected_option
-from .config import coordenada
 from .validador import Validador
 
 class GUI:
@@ -46,7 +54,7 @@ class GUI:
         self.menu.pack(side="left", fill="y", padx=10, pady=10)
 
     #Botões
-        self.botao_save = criar_botao(self.janela, "Salvar Coordenada", self.salvar_coor)
+        self.botao_save = criar_botao(self.janela, "Salvar Coordenada", self.salvar_coord_info)
 
         self.botao_show = criar_botao(self.menu, "Ver Coordenadas", command=self.show_coor)
 
@@ -77,15 +85,17 @@ class GUI:
         self.janela.mainloop()
 
     def coletar_coor(self) -> tuple:
-        """Método para coletar as coordenadas."""
-        return (
-            self.server_entry.get(),
-            self.dimension_entry.get(),
-            self.local_entry.get(),
-            float(self.x_entry.get()),
-            float(self.y_entry.get()),
-            float(self.z_entry.get())
+        """Método para coletar as coordenadas.""" #TODO: Refactor
+        new_server = ServidorCreate(nome=self.server_entry.get())
+        new_dimension = DimensaoCreate(nome=self.dimension_entry.get())
+        new_local = LocalCreate(nome=self.local_entry.get())
+        new_coord = CoordenadaCreate(
+            x=float(self.x_entry.get()),
+            y=float(self.y_entry.get()),
+            z=float(self.z_entry.get())
         )
+
+        return (new_server, new_dimension, new_local, new_coord)
 
     def limpar_campos(self):
         """Limpa os campos de entrada."""
@@ -99,47 +109,39 @@ class GUI:
             entry.delete(0, "end")
         return
 
-    def salvar_coor(self):
-        """Método para coletar as coordenadas."""
-        server, dim, local, x, y, z = self.coletar_coor()
+    def salvar_coord_info(self):
+        """Método para salvar as coordenadas."""
+        server_schema, dimension_schema, local_schema, coordenada_schema = self.coletar_coor()
 
-        if not self.validate_entries(server, dim, local, x, y, z):
+        if not self.validate_entries(server_schema.nome, dimension_schema.nome, local_schema.nome, coordenada_schema):
             self.janela.after(2000, self.limpar_mensagem) # 2000ms = 2 segundos
 
-        if check_existence(server_name=server, dim_name=dim, local_name=local, coor_x=x, coor_y=y, coor_z=z):
-            self.dados.configure(text=f'Coordenada "{local}" já existente!')
-            self.limpar_campos()
-            self.janela.after(3000, self.limpar_mensagem)
-
-    #Salva as coordenadas
         else:
-            save_coord = commit_coordenada(
-                srv_name=server,
-                dim_name=dim,
-                local_name=local,
-                coor_x=x,
-                coor_y=y,
-                coor_z=z
-            )
-            self.dados.configure(text=save_coord)
-            self.limpar_campos()
+            servidor = commit_servidor(server_schema)
+            dimensao = commit_dimensao(dimension_schema, servidor.id)
+            local = commit_local(local_schema, servidor.id, dimensao.id)
+            coordenada = commit_coordenada(coordenada_schema, servidor.id, dimensao.id, local.id)
 
-    def validate_entries(self, server, world, nome, coor_x, coor_y, coor_z) -> bool:
+            self.limpar_campos()
+            self.dados.configure(text=f'Coordenada "{local.nome}" salva com sucesso!')
+            self.janela.after(2000, self.limpar_mensagem)
+
+    def validate_entries(self, server: Servidor, world: Dimensao, nome: Local, coordinate: Coordenada) -> bool:
         """Método para validar as entradas."""
         valid_serv = Validador.validar_string(server, "Servidor")
         valid_wrld = Validador.validar_world(world, "Dimensão")
         valid_nome = Validador.validar_string(nome, "Nome da Coordenada")
-        valid_coord_x = Validador.validar_coord(coor_x, "Coordenada X")
-        valid_coord_y = Validador.validar_coord(coor_y, "Coordenada Y")
-        valid_coord_z = Validador.validar_coord(coor_z, "Coordenada Z")
+        valid_cx = Validador.validar_coord(coordinate.x, "Coordenada X")
+        valid_cy = Validador.validar_coord(coordinate.y, "Coordenada Y")
+        valid_cz = Validador.validar_coord(coordinate.z, "Coordenada Z")
 
-        validador = [valid_serv, valid_wrld, valid_nome, valid_coord_x, valid_coord_y, valid_coord_z]
+        validador = [valid_serv, valid_wrld, valid_nome, valid_cx, valid_cy, valid_cz]
 
         if not all(validador):
             self.dados.configure(text="Entrada inválida!")
 
         #Verificando se todos os campos foram preenchidos
-        if not world or not nome or not coor_x or not coor_y or not coor_z:
+        if not world or not nome or not coordinate:
             self.dados.configure(text="Todos os campos devem ser preenchidos!")
 
         return True
